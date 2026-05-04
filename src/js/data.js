8508c1e9dc499a42af1bd17f693b0b8187a06ea3
chavrn <charlinevernay04@gmail.com>
2026-05-04 13:32:59 +0200

    Add new data sources for j-1 an

--- src/js/data.js
+++ src/js/data.js
@@ -26,8 +26,14 @@ export const SOURCES_MAP = {
     { label: 'Statistiques violence domestique — EBG', url: 'https://www.ebg.admin.ch/dam/fr/sd-web/YXpdk4fA4TpV/a4_chiffres-de-la-violence-domestique-en-suisse.pdf' },
     { label: 'Campagne nationale — EDI', url: 'https://www.edi.admin.ch/fr/violence-domestique-sexuelle-et-de-genre-la-premiere-campagne-nationale-de-prevention-est-lancee' },
   ],
+  'j-1-an': [
+    { label: 'Infractions violence domestique 2023 — EBG', url: 'https://www.ebg.admin.ch/dam/fr/sd-web/YXpdk4fA4TpV/a4_chiffres-de-la-violence-domestique-en-suisse.pdf' },
+  ],
   'j-2-ans': [
     { label: 'Profil des auteurs de féminicides — 24 Heures', url: 'https://www.24heures.ch/suisse-le-profil-des-auteurs-de-feminicides-190676825350' },
+  ],
+  'j-15-ans': [
+    { label: 'Féminicides recensés — Stop Femizid', url: 'https://www.stopfemizid.ch/francais' },
   ],
   'j-30-ans': [
     { label: 'Infractions violence domestique 2023 — EBG', url: 'https://www.ebg.admin.ch/dam/fr/sd-web/YXpdk4fA4TpV/a4_chiffres-de-la-violence-domestique-en-suisse.pdf' },
